d85cde30f5a6ee9efb49e7306e4e2b1b98f8a299
Ganiu Aderibigbe <aderibigbeganiu@gmail.com>
2025-01-02 12:23:03 +0100

    Add Banners component to Home for enhanced content display

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ActiveCampaignsSection from "../../sections/activeCampaignsSection";
 import AnalyticsSection from "../../sections/analyticsSection";
+import Banners from "./Banners";
 
 const Home: React.FC = () => {
     return (
         <div className="flex flex-col gap-6 laptop:gap-7 pb-16 pt-10">
             <ActiveCampaignsSection />
             <AnalyticsSection />
+            <Banners />
         </div>
     );
 };
