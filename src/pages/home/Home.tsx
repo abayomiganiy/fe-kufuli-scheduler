14d3ad26430547066cabe16fb95c014dd37f5e18
Ganiu Aderibigbe <aderibigbeganiu@gmail.com>
2025-01-02 10:28:09 +0100

    Add padding to Home component for improved layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,7 @@ import AnalyticsSection from "../../sections/analyticsSection";
 
 const Home: React.FC = () => {
     return (
-        <div className="flex flex-col gap-6 laptop:gap-7">
+        <div className="flex flex-col gap-6 laptop:gap-7 pb-16 pt-10">
             <ActiveCampaignsSection />
             <AnalyticsSection />
         </div>
