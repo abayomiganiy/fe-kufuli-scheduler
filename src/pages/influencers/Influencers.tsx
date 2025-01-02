3f659947f5b991ab0563210fe6678b4280095f9d
Ganiu Aderibigbe <aderibigbeganiu@gmail.com>
2025-01-02 13:05:49 +0100

    Remove SectionHeader component from Influencers page for simplification

--- src/pages/influencers/Influencers.tsx
+++ src/pages/influencers/Influencers.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import SectionHeader from "../../components/sectionHeader";
 
 const Influencers: React.FC = () => {
     return (
         <div>
-            <SectionHeader title="Active Campaigns" />
             Influencers
         </div>
     );
