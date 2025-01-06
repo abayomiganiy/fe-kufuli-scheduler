7ceba0d76ab17f739d6ce6e7a3bba5cab2e98b3f
Ganiu Aderibigbe <aderibigbeganiu@gmail.com>
2025-01-06 10:49:18 +0100

    Enhance ActiveCampaign component to display view count with icon and improve layout structure

--- src/components/activeCampaigns/ActiveCampaigns.tsx
+++ src/components/activeCampaigns/ActiveCampaigns.tsx
@@ -130,9 +130,30 @@ const ActiveCampaign: React.FC<{ campaign: ICampaign }> = ({ campaign }) => {
                 <div className="absolute top-0 left-0 w-full h-full rounded-2xl bg-gradient-to-t from-0 from-black to-50% to-transparent opacity-80" />
             )}
             <div className="z-40">
-                <h3 className="text-sm font-medium line-clamp-1">
-                    {(campaign.content[0] as CreateImageMessage).caption}
-                </h3>
+                <div>
+                    <div className="flex justify-center items-center space-x-1">
+                        <svg
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="font-semibold text-xs laptop:text-base h-4 w-4 laptop:h-6 laptop:w-6"
+                        >
+                            <path
+                                d="M21.544 11.045C21.848 11.4713 22 11.6845 22 12C22 12.3155 21.848 12.5287 21.544 12.955C20.1779 14.8706 16.6892 19 12 19C7.31078 19 3.8221 14.8706 2.45604 12.955C2.15201 12.5287 2 12.3155 2 12C2 11.6845 2.15201 11.4713 2.45604 11.045C3.8221 9.12944 7.31078 5 12 5C16.6892 5 20.1779 9.12944 21.544 11.045Z"
+                                stroke="white"
+                                stroke-width="1.5"
+                            />
+                            <path
+                                d="M15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12Z"
+                                stroke="white"
+                                stroke-width="1.5"
+                            />
+                        </svg>
+                        <h3 className="text-sm font-medium line-clamp-1">
+                            {(campaign.content[0] as CreateImageMessage).views}
+                        </h3>
+                    </div>
+                </div>
             </div>
         </div>
     );
