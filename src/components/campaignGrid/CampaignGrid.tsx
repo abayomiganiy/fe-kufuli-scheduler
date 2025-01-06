dc20d0aebb9f890f3f92c2383aa5aa50f6c24bd4
Ganiu Aderibigbe <aderibigbeganiu@gmail.com>
2025-01-06 11:25:14 +0100

    Enhance CampaignGrid component to display upcoming status and view count with improved layout

--- src/components/campaignGrid/CampaignGrid.tsx
+++ src/components/campaignGrid/CampaignGrid.tsx
@@ -239,8 +239,11 @@ const CampaignGrid: React.FC<{ campaign: ICampaign }> = ({ campaign }) => {
                 <div className="absolute bottom-3 right-3">{campaignIcon}</div>
                 {content}
             </div>
-            <div className="w-full flex flex-col items-start justify-start gap-2">
-                <Toggle isOn={campaign.status === "active"} />
+            <div className="w-full flex flex-col gap-2">
+                <div className="flex justify-between items-center">
+                    <Toggle isOn={campaign.status === "active"} />
+                    <div className="bg-[#F2F4FC] text-[#205CE2] font-bold text-xs rounded py-1 px-2">Upcoming</div>
+                </div>
                 <div className="font-medium text-xs laptop:text-sm line-clamp-2">
                     {(campaign.content[0] as CreateImageMessage).caption ??
                         (
@@ -249,7 +252,26 @@ const CampaignGrid: React.FC<{ campaign: ICampaign }> = ({ campaign }) => {
                                 | CreateTextStoryData
                         ).text}
                 </div>
-                <div></div>
+                <div className="flex items-center gap-2 mt-auto">
+                    <div className="flex justify-center items-center gap-1">
+                        <h3 className="font-semibold text-xs laptop:text-sm">
+                            {campaign.content[0].views}
+                        </h3>
+                        <span className="font-light text-xs laptop:text-sm">
+                            views
+                        </span>
+                    </div>
+                    <div className="flex justify-center items-center gap-1">
+                        <h5 className="font-medium text-xs laptop:text-sm">
+                            {"Daily"}
+                        </h5>
+                    </div>
+                    <div className="flex justify-center items-center gap-1">
+                        <h5 className="font-medium text-xs laptop:text-sm">
+                            {"5:30pm"}
+                        </h5>
+                    </div>
+                </div>
             </div>
         </div>
     );
