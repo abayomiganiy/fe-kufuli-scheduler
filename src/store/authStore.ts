f02da5451cee385ac3b5a854d878469f2283b91a
Ganiu Aderibigbe <aderibigbeganiu@gmail.com>
2025-05-22 12:36:01 +0100

    Update zustand version and enhance dev script in package.json

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,13 +1,21 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-interface IAuth {
+interface IAuthStore {
     token?: string | null;
     login: (token: string) => void;
     logout: () => void;
 }
 
-export const useAuthStore = create<IAuth>((set) => ({
-    token: undefined,
-    login: (token: string | null) => set({ token }),
-    logout: () => set({ token: null }),
-}));
+export const useAuthStore = create<IAuthStore>()(
+    persist(
+        (set) => ({
+            token: undefined,
+            login: (token: string | null) => set({ token }),
+            logout: () => set({ token: null }),
+        }),
+        {
+            name: "auth-storage",
+        }
+    )
+);
